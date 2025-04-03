c56b6bf46a2d9c6dcf002e0fd7d0e08fdb14f84d
Nephty <arnaud.moreau@outlook.com>
2025-04-03 18:43:41 +0200

    fix import error

--- familybusiness/account/forms.py
+++ familybusiness/account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-from problem.exception import ValidationError
+from django.core.exceptions import ValidationError
 
 from .models import Account
 
